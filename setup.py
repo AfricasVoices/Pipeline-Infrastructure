15c5d9a4b065ff6b5778c7dd2c979f545344acfd
Alexander Simpson <as2388@cam.ac.uk>
2021-11-19 16:10:58 +0000

    Version bump for release v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PipelineInfrastructure",
-    version="0.1.1",
+    version="0.1.2",
     python_requires='>=3.6.0',
     url="https://github.com/AfricasVoices/Pipeline-Infrastructure",
     packages=find_packages(exclude=("test",)),
