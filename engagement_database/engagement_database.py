b8b8e0affed48945cadf903bd0bf31584e3902b8
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-12-16 10:04:37 +0300

    format code

--- engagement_database/engagement_database.py
+++ engagement_database/engagement_database.py
@@ -286,7 +286,7 @@ class EngagementDatabase(object):
                             to be explicitly committed elsewhere.
         :type transaction: google.cloud.firestore.Transaction | None
         """
-        message = self.get_message(message_id, transaction)
+        message = self.get_message(message_id, transaction=transaction)
         history_entries = self.get_history_for_message(message_id, transaction=transaction)
         
         if transaction is None:
